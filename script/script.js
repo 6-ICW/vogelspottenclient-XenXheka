748f467e219089cacb67710ba6a2945b13f87cd7
hdktsaam <hannes.dekesel@tsaam.be>
2025-12-09 09:23:29 +0100

    feedback

--- script/script.js
+++ script/script.js
@@ -1,43 +1,39 @@
-const plusbtn = document.querySelector("#plusBtn")
-const select = document.querySelector("#vogelSelect")
-let lijstvogels = [] 
-const counttext = document.getElementById("count")
+const plusbtn = document.querySelector("#plusBtn");
+const select = document.querySelector("#vogelSelect");
+let lijstvogels = [];
+const counttext = document.getElementById("count");
 fetch("https://apivogelspotten.onrender.com/vogelspotten/")
-.then((info) => info.json())
-.then((data) => {    
-    
-    const aantalkeergespot = 0
-        
-    
-    data.forEach((vogel) => {
-        let niewvogel = 
-        {
-        id:vogel.id,
-        soort:vogel.soort,
-        gespot:aantalkeergespot}
-        lijstvogels.push(niewvogel)    
-    })
-    console.log(lijstvogels);
-    lijstvogels.forEach((vogel) => {
-        const option = document.createElement("option")
-        option.value = vogel.id
-        option.innerHTML = vogel.soort
-        select.appendChild(option)
-        
-     })
-     
-        select.addEventListener("change",(event) => {
-            const id = event.target.value
-            const gevondenVogel = lijstvogels.find((vogel) => vogel.id == id)            
-            console.log(gevondenVogel);
-            counttext.innerText = gevondenVogel.gespot
-            plusbtn.addEventListener("click",()=> {
-                gevondenVogel.gespot ++
-                counttext.innerText = gevondenVogel.gespot
-        })
-        })
-    
-    
-        
-})
+  .then((info) => info.json())
+  // maak geen gebruik van data hier! 
+  .then((data) => {
+    const aantalkeergespot = 0;
 
+    data.forEach((vogel) => {
+      let niewvogel = {
+        id: vogel.id,
+        soort: vogel.soort,
+        gespot: aantalkeergespot,
+      };
+      lijstvogels.push(niewvogel);
+    });
+    console.log(lijstvogels);
+
+    // data bevat de lijst met vogels, waarom dan lijstvogels?
+    lijstvogels.forEach((vogel) => {
+      const option = document.createElement("option");
+      option.value = vogel.id;
+      option.innerHTML = vogel.soort;
+      select.appendChild(option);
+    });
+
+    select.addEventListener("change", (event) => {
+      const id = event.target.value;
+      const gevondenVogel = lijstvogels.find((vogel) => vogel.id == id);
+      console.log(gevondenVogel);
+      counttext.innerText = gevondenVogel.gespot;
+      plusbtn.addEventListener("click", () => {
+        gevondenVogel.gespot++;
+        counttext.innerText = gevondenVogel.gespot;
+      });
+    });
+  });
